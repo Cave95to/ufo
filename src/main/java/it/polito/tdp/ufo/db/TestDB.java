59310402d8be5a882bd58fc9449d7d33fc109f1e
andre <andre@DESKTOP-PMTSH1N.lan>
2021-03-22 10:59:44 +0100

    2 prova con inserimento query param, PreparedStatement NO interf grafica

--- src/main/java/it/polito/tdp/ufo/db/TestDB.java
+++ src/main/java/it/polito/tdp/ufo/db/TestDB.java
@@ -2,6 +2,7 @@ package it.polito.tdp.ufo.db;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -19,6 +20,7 @@ public class TestDB {
 			//stabiliamo connessione
 			Connection conn = DriverManager.getConnection(jdbcURL);
 			
+			/*
 			//creiamo statement
 			Statement st = conn.createStatement();
 			
@@ -26,6 +28,14 @@ public class TestDB {
 			String sql = "SELECT DISTINCT shape FROM sighting WHERE shape <>'' ORDER BY shape";
 			
 			ResultSet res = st.executeQuery(sql);
+			*/
+			
+			//CREIAMO QUERY DINAMICHE
+			
+			//questa è ancora statica ma cambia ordine righe perché usiamo preparedStatement rispetto a prima
+			String sql = "SELECT DISTINCT shape FROM sighting WHERE shape <>'' ORDER BY shape";
+			PreparedStatement st = conn.prepareStatement(sql);
+			ResultSet res = st.executeQuery();
 			
 			//processiamo risultato
 			List<String> formeUfo = new ArrayList<>();
@@ -37,6 +47,27 @@ public class TestDB {
 			}
 			
 			System.out.println(formeUfo);
+			
+			//nuova query dinamica
+			// ? per ogni valore incognito
+			String sql2 = "SELECT COUNT(*) AS cnt FROM sighting WHERE shape = ? ";
+			
+			// non abbiamo finestra per utente quindi facciamo così per ora
+			String shapeScelta = "circle";
+			
+			PreparedStatement st2 = conn.prepareStatement(sql2);
+			
+			// andiamo a sostituire i valori inseriti al posto dei ?
+			// 1 indica il numero di ? che stiamo sostituendo
+			st2.setString(1, shapeScelta);
+			
+			ResultSet res2 = st2.executeQuery();
+			
+			//sappiamo di avere 1 sola riga nel risultato. first o next sarebbe uguale
+			res2.first();
+			
+			int count = res2.getInt("cnt");
+			System.out.println("Ufo di forma "+shapeScelta+" sono "+count);
 			
 			//chiudiamo connessione
 			conn.close();
